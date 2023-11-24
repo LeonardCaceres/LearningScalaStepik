a02b3b6a15aa2df99f43f86236f2314c176e05a1
Caceres Leonard <125807225+LeonardCaceres@users.noreply.github.com>
2023-11-25 01:23:16 +0300

    Create Learning2.scala

---
+++ Learning2.scala
@@ -0,0 +1,46 @@
+//object FIrst_normal extends App{
+//  val mes = "Hello world!"
+//  println(mes)
+//}
+
+//object FIrst_normal extends App {
+//  import scala.io.StdIn.readLine
+//  println("THIS TEXT WRITE")
+//  val line = readLine()
+//  print("you input" + line + '\n')
+//  print(line)
+//}
+
+//import scala.io.StdIn
+//object Main {
+//  def main(args: Array[String]): Unit = {
+//    val name = StdIn.readLine()
+//    print("Hi, " + name + '!')
+//    val x: Int = 1
+//  }
+//}
+
+//import scala.io.StdIn
+//object Main extends App {
+//  val x = StdIn.readInt()
+//  val y = StdIn.readInt()
+//  val z = StdIn.readInt()
+//  println(x * y * z)
+//}
+
+//import ru.NAMEZZZZZZZZZZ.{name_oleg => oleg, name_leo => leonard}
+//import ru.NAMEZZZZZZZZZZ._
+
+// выброс при импорте
+//import ru.NAMEZZZZZZZZZZ.{name_oleg => _, _}
+//
+//object Test extends App {
+////  val nameT = name_oleg
+////  println(nameT)
+//  val nameT2 = name_leo
+//  println(nameT2)
+////val nameT = oleg
+////println(nameT)
+////val nameT2 = leonard
+////println(nameT2)
+//}
